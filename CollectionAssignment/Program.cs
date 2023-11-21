aace04197ec1d31d6970c6ef804f49751a32a3ad
Miika Reijonen <hmire02@xamk.fi>
2023-11-21 10:11:17 +0200

    Add project files.

---
+++ CollectionAssignment/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment1();
+            Assignment2();
+            Assignment3();
+            Assignment4();
+            Assignment5();
+            Assignment6();
+        }
+
+        static void Assignment1()
+        {
+            // Create a new array or list
+
+            // Add numbers 0-9 to the array or list using a for loop or foreach loop
+
+            // Print the array or list
+        }
+
+        static void Assignment2()
+        {
+            // Create a new array or list with numbers 0-9
+
+            // Print to the user numbers that are greater than 5 using ForEach loop
+        }
+
+        static void Assignment3()
+        {
+            // Ask the user for 5 integers and store them in an array or list
+
+            // Then tell the user what the average is
+            // Calculate the average using ForEach or For loop
+            // There is a function that calculates the average (Average), but do not use it, calculate it manually.
+            // This helps understand how the average is calculated and how the Average function works.
+        }
+
+        static void Assignment4()
+        {
+            // Ask the user which name they want to remove from the list
+            // The user can remove a name from the list either by writing the name or a number
+            // If the user writes a number, then remove the name based on the array index
+            // In this case, the user can input the numbers 0,1,2
+            // After this, print the list again and show that the name has been removed
+
+            List<string> nameList = new List<string>
+            {
+                "Jonathan",
+                "Suzi",
+                "Joseph"
+            };
+        }
+
+        static void Assignment5()
+        {
+            // Create and Print a Dictionary
+
+            // Create a Dictionary where the keys are people's names (as strings) and values are their ages (as integers).
+            // Add three people to the dictionary.
+            // Print each person and their age.
+        }
+
+        static void Assignment6()
+        {
+            // Implement a program that allows the user to add, remove, and edit people using a dictionary.
+            // First, ask the user whether they want to add, remove, or edit people.
+            // Then call the correct function that implements the functionality.
+            // The user can exit the program by typing "exit" if they are in the "main menu",
+            // i.e., at the beginning of this function where the action is asked.
+
+            Dictionary<string, int> personsDictionary = new Dictionary<string, int>();
+            personsDictionary.Add("Jonathan", 21);
+            personsDictionary.Add("Suzi", 25);
+            personsDictionary.Add("Joseph", 32);
+
+            //Implement add
+            Assignment6Add(personsDictionary);
+
+            //Implement remove
+            Assignment6Remove(personsDictionary);
+
+            //Implement edit
+            Assignment6Edit(personsDictionary);
+
+        }
+
+        static void Assignment6Add(Dictionary<string, int> persons)
+        {
+            // Make a program that asks the user for a name and age. Store this result in a dictionary,
+            // where the key is the name and the value is the age.
+
+            // Then print all names and ages.
+
+            // The user can stop adding by typing "exit"
+        }
+
+        static void Assignment6Remove(Dictionary<string, int> persons)
+        {
+            // Extend Assignment6 task so that the user can also remove people from the dictionary.
+            // The user can remove based on either name or age.
+            // The user can stop removing by typing "exit"
+        }
+
+        static void Assignment6Edit(Dictionary<string, int> persons)
+        {
+            // Then make a functionality where the user can edit a person's age.
+            // Age can be edited if the name given by the user is found in the dictionary.
+            // Then ask the user for a new age.
+            // If the person is not found, print an error message.
+            // Then ask the user for a name again.
+            // The user can stop editing by typing "exit"
+        }
+    }
+}
---
+++ ReactionTimeAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace ReactionTimeAssignment
+{
+    internal class Program
+    {
+        // Create an application that measures the user's reaction time.
+        // Record 3-5 reaction times from the user and then inform them of their average reaction speed.
+        // Additionally, you can implement a High Score feature using a Dictionary.
+        // Store in the Dictionary the user's name as the key and their reaction time as the value.
+        // Display the High Score list after each completed game.
+        // The High Score list can only have five names at a time.
+        // If a new result is better than the ones on the list, remove the worst score from there and add the new one.
+        // Also, the High Score list should always be printed so that the best player is first.
+
+        // The user can then continue to play or exit the program.
+
+        // NOTE: You are free to implement the program the way you like, but it's good idea to use this exercise to
+        //       practice using methods too.
+
+        static void Main(string[] args)
+        {
+            // Create a new random object
+            Random random = new Random();
+            // Set a random time between 1-5 seconds.
+            int sleepTime = random.Next(1000, 5000);
+            // Wait for a random time between 1-5 seconds.
+            Thread.Sleep(sleepTime);
+
+            // Start the timer.
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            // Stop the timer.
+            stopwatch.Stop();
+            // Record the reaction time.
+            double reactionTime = stopwatch.Elapsed.TotalMilliseconds;
+        }
+    }
+}
